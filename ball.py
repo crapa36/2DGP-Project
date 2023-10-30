ec8db07ee4815f14b44c7cc7ca14178baac3ad0a
crapa-Desktop <crapa36@gmail.com>
2023-10-30 16:51:57 +0900

    공 높이 구현

--- ball.py
+++ ball.py
@@ -9,10 +9,10 @@ class Ball:
     def __init__(self, x=400, y=300, x_velocity=1, y_velocity=1):
         if Ball.image is None:
             Ball.image = load_image("ball.png")
-        # if Ball.shadowImage is None:
-        # Ball.shadowImage = load_image("ball_shadow.png")
+        if Ball.shadowImage is None:
+            Ball.shadowImage = load_image("ball_shadow.png")
         self.x, self.y, self.x_velocity, self.y_velocity = x, y, x_velocity, y_velocity
-        self.frame, self.height = 0, 3
+        self.frame, self.height, self.height_velocity = 0, 5, 1.5
 
     def draw(self):
         frame_width = 10
@@ -29,7 +29,11 @@ class Ball:
         self.image.clip_draw(
             frame_x, frame_y, frame_width, frame_height, self.x, self.y
         )
-        # self.shadowImage.draw(self.x, self.y - self.height)
+        self.shadowImage.draw(self.x, self.y - self.height)
+        self.height += self.height_velocity
+        self.height_velocity -= 0.1
+        if self.height <= 0:
+            self.height_velocity = 2
 
     def update(self):
         self.x += self.x_velocity
